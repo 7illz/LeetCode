d3368020749cf35567cbd274a9371f356c39701f
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-08-08 23:43:08 +0600

    Time: 4 ms (50.1%), Space: 15.1 MB (5.94%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        dic={}
+        for i in nums:
+                    if i not in dic:
+                        dic[i]=1
+                    else:
+                        dic[i]+=1
+        for key, value in dic.items():
+            if value == 1:
+                return key
+
+        
