2e5058efae3d22b6a909809ca803b60eaa904cb9
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-08-09 00:55:17 +0600

    Time: 6 ms (71.64%), Space: 13.7 MB (17.26%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+
+        dic={}
+        for i in nums:
+            if i not in dic:
+                dic[i]=1
+            else:
+                dic[i]+=1
+        for key,value in dic.items():
+            if value>len(nums)/2:
+                return key
+
+
+        
