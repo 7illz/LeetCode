675f884d98caa5dcdd233ccf776c11d772b404ed
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-08-10 07:19:15 +0600

    Time: 519 ms (41.97%), Space: 13.3 MB (36.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-            for i in range(len(nums)):
-                for j in range(i+1,len(nums)):
-                    if nums[i]+nums[j]==target:
-                        return [i,j]
+        dic={}
+        for i in range(len(nums)):
+            if target-nums[i] not in dic.keys():
+                dic[nums[i]]=i
+            else:
+                return [dic[target-nums[i]],i]
+                
 
 
 
