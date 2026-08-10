05d28abeca5982570d6ddecdf3b09135d81448a6
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-08-10 21:19:35 +0600

    Time: 7 ms (17.31%), Space: 19.9 MB (20.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,5 +1,13 @@
 class Solution(object):
     def reverseString(self, s):
         
-        s.reverse()
+        l=0
+        r = len(s) - 1
+        while l<r:
+            temp=s[r]
+            s[r]=s[l]
+            s[l]=temp
+            r-=1
+            l+=1
+             
         
