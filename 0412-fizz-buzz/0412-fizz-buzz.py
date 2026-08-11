0ee0dbc4ce3a1fbbc7968f91cbd449875b426426
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-08-11 22:19:46 +0600

    Time: 0 ms (100%), Space: 13.2 MB (78.07%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        arr=[]
+        for i in range(n):
+            if (i+1)%3==0 and (i+1) %5==0 :
+                arr.append('FizzBuzz')
+            elif (i+1) %3==0 :
+                arr.append('Fizz')
+
+            elif (i+1) %5==0 :
+                arr.append('Buzz')
+
+            else:
+                arr.append(str(i+1))
+
+        return arr
+
+            
+
+                
+
+
+
+
+
+        
