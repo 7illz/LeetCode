8c38f7fbd7c1d1e2efde4444d2625ee0113ca8b5
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-08-29 17:45:42 +0600

    Time: 0 ms (100%), Space: 12.6 MB (30.69%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,15 +1,29 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
 
-        lst=[]
-        new=[]
-        for i in range(len(nums1)):
-            if nums1[i] not in lst:
-                lst.append(nums1[i])
+        # lst=[]
+        # new=[]
+        # for i in range(len(nums1)):
+        #     if nums1[i] not in lst:
+        #         lst.append(nums1[i])
 
-        for i in range(len(nums2)):
-            if nums2[i] in lst:
-                lst.remove(nums2[i])
-                new.append(nums2[i])
+        # for i in range(len(nums2)):
+        #     if nums2[i] in lst:
+        #         lst.remove(nums2[i])
+        #         new.append(nums2[i])
 
-        return new
+        # return new
+
+
+
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        values = set(nums1)
+        result = set()
+
+        for number in nums2:
+            if number in values:
+                result.add(number)
+
+        return list(result)
