eaf668871de5cf685e1ebd06bf25c7aab72a3045
SHATIL AHMED CHOWDHURY <shatil.ahmed.chowdhury@g.bracu.ac.bd>
2026-09-03 18:14:39 +0600

    Time: 131 ms (27.65%), Space: 19 MB (49.34%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        l=0
+        r=1
+        max_prof=0
+        while r<len(prices):
+
+            max_prof=max(max_prof,(prices[r]-prices[l]))
+            if prices[l]>=prices[r]:
+                l=r
+
+
+            r+=1
+        if max_prof<0:
+            max_prof=0
+        return max_prof
+        
